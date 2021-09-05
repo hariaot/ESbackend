07a40ff367adb421c4c3444d3ae3fd37a7d6897e
Hariaot <haria.othon99@gmail.com>
2021-09-05 17:21:34 -0300

    iniciando back end

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'Vo@dores22',
+    database: 'databaseUniv',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
