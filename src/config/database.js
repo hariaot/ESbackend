578ce40c44732685a629074329b714421af18f56
Hariaot <haria.othon99@gmail.com>
2021-09-05 17:55:49 -0300

    atualizando config

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'mysql',
     host: 'localhost',
     username: 'root',
-    password: 'Vo@dores22',
+    password: 'rootpassword',
     database: 'databaseUniv',
     define: {
         timestamps: true,
